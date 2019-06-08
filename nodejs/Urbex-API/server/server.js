c47aa18731fd20d60478ba37a17d789cd6544345
Corey Zarkowski <coreyz206@gmail.com>
2019-06-08 16:42:06 -0700

    trying this

--- nodejs/Urbex-API/server/server.js
+++ nodejs/Urbex-API/server/server.js
@@ -1,40 +1,15 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
-// Node module: loopback-workspace
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-'use strict';
-
-var loopback = require('loopback');
-var boot = require('loopback-boot');
 var https = require('https');
 var sslConfig = require('./ssl-config');
+//...
 var options = {
   key: sslConfig.privateKey,
   cert: sslConfig.certificate
-}
+};
+//...
 
-var app = module.exports = loopback();
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
+server.listen(app.get('port'), function() {
     var baseUrl = (httpOnly? 'http://' : 'https://') - app.get('host') - ':' - app.get('port');
     app.emit('started', baseUrl);
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
 });
+return server;
