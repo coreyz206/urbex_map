f956a1b46e58c7d65d8ac28c9ef4c8a69e9712ca
Corey Zarkowski <coreyz206@gmail.com>
2019-06-10 15:46:21 -0700

    trying some new stuff with routes.js part final hopefully

--- nodejs/Urbex-API/server/boot/routes.js
+++ nodejs/Urbex-API/server/boot/routes.js
@@ -5,13 +5,11 @@
 
 'use strict';
 
-var path = require('path');
-
 module.exports = function(server) {
   // Install a `/` route that redirects to map
   var router = server.loopback.Router();
   router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '../client/map_main.html'));
+    res.sendFile('../../client/map_main.html');
   });
   server.use(router);
 };
