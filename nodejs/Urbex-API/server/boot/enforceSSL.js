52e55f8ed1d68251035a812c68122aa5a4ac5dbb
Corey Zarkowski <46543468+coreyz206@users.noreply.github.com>
2019-07-23 11:29:52 -0700

    Update enforceSSL.js

--- nodejs/Urbex-API/server/boot/enforceSSL.js
+++ nodejs/Urbex-API/server/boot/enforceSSL.js
@@ -6,7 +6,7 @@ var app = loopback();
 
 module.exports = function() {
   var forceSsl = function(req, res, next) {
-    if (req.headers["x-forwarded-proto"] !== "https") {
+    if (req.headers["forwarded-proto"] !== "https") {
       return res.redirect(["https://", req.get("Host"), req.url].join(""));
     }
     return next();
