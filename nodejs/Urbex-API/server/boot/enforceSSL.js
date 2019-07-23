e98702026a6ec67d70386d9aa48b096e82cddc97
Corey Zarkowski <46543468+coreyz206@users.noreply.github.com>
2019-07-23 09:52:26 -0700

    Update and rename forceSSL.js to enforceSSL.js

--- nodejs/Urbex-API/server/boot/enforceSSL.js
+++ nodejs/Urbex-API/server/boot/enforceSSL.js
@@ -5,14 +5,12 @@ var path = require('path');
 module.exports = function(server) {
   var forceSsl = function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     return next();
   };
 
-	app.configure(function () {
-    if (env === 'production') {
-        app.use(forceSsl);
-    }
+	app.configure(function() {
+     app.use(forceSsl);
 	}
 };
