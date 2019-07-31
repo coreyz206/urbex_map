54ace165619eef29ec3e09851c2807d4da00b55d
Corey Zarkowski <46543468+coreyz206@users.noreply.github.com>
2019-07-31 15:53:58 -0700

    refactored automigrate script

--- nodejs/Urbex-API/server/automigrate.js
+++ nodejs/Urbex-API/server/automigrate.js
@@ -1,9 +1,11 @@
-var server = require('./server.js');
-var ds = server.dataSources.locationDS;
-var lbTable = 'locations';
-
-ds.automigrate(lbTable, function(er) {
-    if(er) throw er;
-    console.log('Loopback table [' + lbTable + '] has been created in ', ds.adapter.name);
-    ds.disconnect;
+var server = require("./server");
+var ds = server.dataSources.db;
+var lbTables = ["User", "AccessToken", "ACL", "RoleMapping", "Role"];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log(
+    "Loopback tables [" + lbTables + "] created in ",
+    ds.adapter.name
+  );
+  ds.disconnect();
 });
