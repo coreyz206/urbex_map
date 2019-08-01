2bb88c0986b9f123b4df12dba4dc0faa967fcdfd
Kevin <kev.favilla@gmail.com>
2019-07-31 21:51:58 -0700

    Update for server deployment
    Changed dBName parameter to "urbex"

--- PHP Urbex - Copy/includes/dbh.inc.php
+++ PHP Urbex - Copy/includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "loginsystem"
+$dBName = "urbex"
     
     $conn = mysqli_connect($servername, $dBUsername, dBPassword, dBName,)
     
